322c4a55b69d675a840540faf115315c7dbbf915
Martin <m.tarantoviez@alumno.um.edu.ar>
2025-04-15 00:09:06 -0300

    palabras no palindromes

--- tests/test.py
+++ tests/test.py
@@ -17,5 +17,14 @@ class TestPalindromesFrases(unittest.TestCase):
     def test_palindrome_nolemon(self):
         self.assertTrue(is_palindrome("ama ala ama"))
 
+class TestFrasesNoPalindromas(unittest.TestCase):
+
+    def test_frase_con_simbolos(self):
+        self.assertFalse(is_palindrome("caminando ando"))
+    def test_frase_larga(self):
+        self.assertFalse(is_palindrome("computacion"))
+    def test_frase_casi_palindromo(self):
+        self.assertFalse(is_palindrome("Peron"))
+
 if __name__ == '__main__':
     unittest.main()
