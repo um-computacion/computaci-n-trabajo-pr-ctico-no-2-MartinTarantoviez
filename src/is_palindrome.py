fbdb33692541643ff0674bb6b48560ca78112b0e
Martin <m.tarantoviez@alumno.um.edu.ar>
2025-04-15 00:35:45 -0300

    limpieza de texto

--- src/is_palindrome.py
+++ src/is_palindrome.py
@@ -1,6 +1,16 @@
 def is_palindrome(value):
-        
-    if value ==  value[::-1]:
-        return True
-    else:
-        return False
+    texto = texto.lower()
+    reemplazos = {
+        'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+        'ü': 'u', 'ñ': 'n'
+    }
+
+    texto_limpio = []
+    for caracter in texto:
+        if caracter in reemplazos:
+            texto_limpio.append(reemplazos[caracter])
+        elif caracter.isalnum():
+            texto_limpio.append(caracter)
+
+    texto_final = ''.join(texto_limpio)
+    return texto_final == texto_final[::-1]
